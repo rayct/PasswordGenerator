ea9e1315759c78da1b83512fe32bfaee63534ee6
rayct <rayct@icloud.com>
2022-06-24 18:31:21 +0100

    Completed Password Generator Password Project

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ import random
 print('Welcome to Your Password Generator Ray')
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ!@£$%^&*().,?0123456789'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input Your Password Length: ')
+length = int(length)
+
+print('\nHere are your passwords:')
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+        print(passwords)
