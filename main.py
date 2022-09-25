533d1485514ee8fe8fb7dc6e0aaa483a96fee070
rayct <rayct@icloud.com>
2022-09-25 23:32:57 +0100

    Syntax change to Camel Case

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ print('Welcome to Your Password Generator Ray')
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ!@£$%^&*().,?0123456789'
 
-number = input('Amount of passwords to generate: ')
+number = input('Amount of Passwords to Generate: ')
 number = int(number)
 
 length = input('Input Your Password Length: ')
 length = int(length)
 
-print('Here are your passwords:')
+print('Here are your Passwords Ray:')
 
 for pwd in range(number):
     passwords = ''
